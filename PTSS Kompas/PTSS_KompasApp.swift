f5bbd5d011ecb8b73b8fae5e234382203a197b7b
DevSoft <devonvanw@gmail.com>
2024-11-19 11:55:12 +0100

    Initial Commit

---
+++ PTSS Kompas/PTSS_KompasApp.swift
@@ -0,0 +1,17 @@
+//
+//  PTSS_KompasApp.swift
+//  PTSS Kompas
+//
+//  Created by Devon van Wichen on 19/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct PTSS_KompasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
